a4a0dc4bf706f2ee7881834f1b98a06a07e8a245
Claudiu Ionita <ionut5002@gmail.com>
2018-03-23 12:02:14 +0000

    profile changes

--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -17,9 +17,9 @@ export class ProfileComponent implements OnInit {
   messageClass;
   message;
   processing = false;
-  form;
+  ChangePassform;
   previousUrl;
-  isReplyFormOpen= false;
+  ChangePassOpen= false;
   
 
   constructor(
@@ -41,21 +41,21 @@ export class ProfileComponent implements OnInit {
     });
   }
   createForm() {
-    this.form = this.formBuilder.group({
+    this.ChangePassform = this.formBuilder.group({
       newPassword: ['', Validators.required], // Username field
       password: ['', Validators.required] // Password field
     });
   }
   // Function to disable form
 disableForm() {
-  this.form.controls['newPassword'].disable(); // Disable username field
-  this.form.controls['password'].disable(); // Disable password field
+  this.ChangePassform.controls['newPassword'].disable(); // Disable username field
+  this.ChangePassform.controls['password'].disable(); // Disable password field
 }
 
 // Function to enable form
 enableForm() {
-  this.form.controls['newPassword'].enable(); // Enable username field
-  this.form.controls['password'].enable(); // Enable password field
+  this.ChangePassform.controls['newPassword'].enable(); // Enable username field
+  this.ChangePassform.controls['password'].enable(); // Enable password field
 }
   onChangePasswordSubmit() {
     this.authService.createAuthenticationHeaders();
@@ -63,8 +63,8 @@ enableForm() {
     this.disableForm(); // Disable form while being process
     // Create user object from user's input
     const CPass = {
-      newPassword: this.form.get('newPassword').value, // Username input field
-      password: this.form.get('password').value // Password input field
+      newPassword: this.ChangePassform.get('newPassword').value, // Username input field
+      password: this.ChangePassform.get('password').value // Password input field
       
     }
   
@@ -79,25 +79,22 @@ enableForm() {
       } else {
         this.messageClass = 'alert alert-success'; // Set bootstrap success class
         this.message = data.message; // Set success message
-        // Function to store user's token in client local storage
-        // After 2 seconds, redirect to dashboard page
         setTimeout(() => {
-          // Check if user was redirected or logging in for first time
-          if (this.previousUrl) {
-            this.router.navigate([this.previousUrl]); // Redirect to page they were trying to view before
-          } else {
-            this.router.navigate(['/dashboard']); // Navigate to dashboard view
-          }
+            this.processing = false;
+            this.ChangePassOpen= false;
+            this.ChangePassform.reset();
+            this.enableForm();
+            window.location.reload()
         }, 2000);
       }
     });
   }
   
 changePasswordBtn(){
-  if(this.isReplyFormOpen){
-    this.isReplyFormOpen = false
+  if(this.ChangePassOpen){
+    this.ChangePassOpen = false
   }else{
-    this.isReplyFormOpen = true
+    this.ChangePassOpen = true
   }
 }
 }
